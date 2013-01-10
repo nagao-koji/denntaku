b32edc018266b214bc24560fe0113faa8d1b0af9
koji nagao <koji19910627@gmail.com>
2013-01-10 14:59:49 +0900

    電卓

--- denntaku/ViewController.h
+++ denntaku/ViewController.h
@@ -8,6 +8,38 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    double x;
+    double y;
+    double Z;
+    int n;
+    int shou;
+    int answer;
+    int keisan;
+    double W;
+    double s;
+}
+@property (weak, nonatomic) IBOutlet UILabel *gamenn;
+/*@property enum {x,y} state;*/
 
+- (IBAction)AC:(id)sender;
+- (IBAction)zero:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+
+
+
+- (IBAction)equal:(id)sender;
+- (IBAction)plus:(id)sender;
+- (IBAction)hiku:(id)sender;
+- (IBAction)kakeru:(id)sender;
+- (IBAction)waru:(id)sender;
+- (IBAction)period:(id)sender;
 @end
--- denntaku/ViewController.m
+++ denntaku/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -25,5 +26,348 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+
+double x=0;
+double y=0;
+double Z=0;
+int n=1;
+int shou=0;
+int answer;
+int keisan=0;
+double W=0;
+double s=0;
+
+
+
+- (IBAction)AC:(id)sender {
+    x=0;
+    y=0;
+    Z=0;
+    shou=0;
+    n=1;
+    W=0;
+    s=0;
+    keisan=0;
+    [[self gamenn] setText:[NSString
+                            stringWithFormat:@"%d", 0]];
+    answer=0;
+}
+/*
+- (void)Button:(int)number{
+    switch(keisan){
+        case 0:
+            
+            if(shou == 0){
+                x=x*10+number;
+                [[self gamenn] setText:[NSString
+                        stringWithFormat:
+                                    @"%g", x]];
+            }
+    
+    
+    else
+    {
+        W=number*pow(10,(-n));
+        
+        s=W+x;
+        [[self gamenn] setText:[NSString
+                                stringWithFormat:@"%g", s]];
+        x=s;
+        n=n+1;
+    }
+            break;
+            
+        case 1:
+            
+            if(shou == 0){
+                y=y*10+number;
+                [[self gamenn] setText:[NSString
+                                    stringWithFormat:@"%g", y]];
+            }
+            
+            
+            else
+            {
+                W=number*pow(10,(-n));
+                
+                s=W+y;
+                [[self gamenn] setText:[NSString
+                                    stringWithFormat:@"%g", s]];
+                y=s;
+                n=n+1;
+            }
+            break;
+    }
+}
+            */
+            
+            
+            
+            
+            
+- (IBAction)zero:(id)sender {
+    switch(keisan) {
+         case 0:
+             x=x*10;
+             [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];             break;
+             
+         case 1:
+             y=y*10;
+         [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+         break;
+     }
+    
+   /* [self Button:0];*/
+}
+
+- (IBAction)one:(id)sender {
+  switch(keisan) {
+        case 0:
+            x=x*10+1;
+          [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+1;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+    /*[self Button:1];*/
+}
+
+- (IBAction)two:(id)sender {
+ switch(keisan) {
+        case 0:
+            x=x*10+2;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+2;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+   /*[self Button:2];*/
+}
+
+- (IBAction)three:(id)sender {
+  switch(keisan) {
+        case 0:
+            x=x*10+3;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+3;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+    
+   /* [self Button:3];*/
+}
+
+- (IBAction)four:(id)sender {
+   switch(keisan) {
+        case 0:
+            x=x*10+4;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+4;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+         break;
+    }
+    
+    /*[self Button:4];*/
+}
+
+- (IBAction)five:(id)sender {
+    switch(keisan) {
+        case 0:
+            x=x*10+5;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+5;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+    
+   /*[self Button:5];*/
+    
+}
+- (IBAction)six:(id)sender {
+   switch(keisan) {
+        case 0:
+            x=x*10+6;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+6;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+    
+    /*[self Button:6];*/
+}
+
+- (IBAction)seven:(id)sender {
+   switch(keisan) {
+        case 0:
+            x=x*10+7;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+7;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+    
+   /* [self Button:7];*/
+}
+
+- (IBAction)eight:(id)sender {
+   switch(keisan) {
+        case 0:
+            x=x*10+8;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+8;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+    
+    /*[self Button:8];*/
+}
+
+- (IBAction)nine:(id)sender {
+    switch(keisan) {
+        case 0:
+            x=x*10+9;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", x]];
+            break;
+            
+        case 1:
+            y=y*10+9;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%g", y]];
+        break;
+    }
+    
+   /* [self Button:9];*/
+}
+
+
+- (IBAction)equal:(id)sender {
+    switch(answer){
+        case 1:
+            
+            Z=x+y;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", Z]];
+            x=Z;
+            y=0;
+            keisan=0;
+            break;
+            
+            
+        case 2:
+            
+            Z=x-y;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g",Z]];
+            x=Z;
+            y=0;
+            keisan=0;
+            break;
+            
+            
+        case 3:
+            
+            Z=x*y;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", Z]];
+            x=Z;
+            y=0;
+            keisan=0;
+            break;
+            
+            
+        case 4:
+            
+            Z=x/y;
+            [[self gamenn] setText:[NSString stringWithFormat:@"%g", Z]];
+            x=Z;
+            y=0;
+            keisan=0;
+            break;
+
+    
+    
+    
+    }
+}
+
+- (IBAction)period:(id)sender {
+    shou=1;
+}
+
+
+
+
+- (IBAction)plus:(id)sender {
+    if (keisan==0){
+        
+        keisan=1;
+        shou=0;
+        n=1;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%d", 0]];
+        answer=1;
+    }
+}
+
+- (IBAction)hiku:(id)sender {
+    if (keisan==0){
+        
+        keisan=1;
+        shou=0;
+        n=1;
+        [[self gamenn] setText:[NSString stringWithFormat:@"%d", 0]];
+        answer=2;
+    }
+}
+
+- (IBAction)kakeru:(id)sender {
+ if (keisan==0){
+        
+        keisan=1;
+        shou=0;
+        n=1;
+        [[self gamenn] setText:[NSString
+                                stringWithFormat:@"%d", 0]];
+        answer=3;
+    }
+}
+
+- (IBAction)waru:(id)sender {
+ if (keisan==0){
+        
+        keisan=1;
+        shou=0;
+        n=1;
+        [[self gamenn] setText:[NSString
+                                stringWithFormat:@"%d", 0]];
+        answer=4;
+    }
+    
+}
+
+
+
 
 @end
